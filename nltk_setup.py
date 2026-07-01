fbe432589c2f4527615e8c34130e5dda90085d3e
Abhishek Kumar <abhishekumar1772004@gmail.com>
2026-07-01 17:26:40 +0530

    Update nltk_setup.py

--- nltk_setup.py
+++ nltk_setup.py
@@ -1,9 +1,15 @@
 import nltk
 
 def download_nltk_resources():
-    resources = ['stopwords', 'punkt', 'wordnet']
-    for resource in resources:
+    resources = {
+        'stopwords': 'corpora/stopwords',
+        'punkt': 'tokenizers/punkt',
+        'wordnet': 'corpora/wordnet',
+    }
+    for name, path in resources.items():
         try:
-            nltk.data.find(f'corpora/{resource}')
+            nltk.data.find(path)
         except LookupError:
-            nltk.download(resource)
+            nltk.download(name, quiet=True)
+
+download_nltk_resources()  # runs immediately when this module is imported
